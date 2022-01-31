03442039d8c1ce98359f691cdb7d6b652578ba0a
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-01-31 16:40:09 +0530

    assignment 1 complete, 2 starter code done

---
+++ Lab Assessment - 2/script.js
@@ -0,0 +1,16 @@
+// Question 2
+
+// Question 3
+const carouselImage = document.getElementById('carousel-image');
+const nextBtn = document.getElementById('next-btn');
+const addBtn = document.getElementById('add-btn');
+
+const imagesArray = [];
+let currentImageIndex = 0;
+
+nextBtn.addEventListener('click', nextButtonClickHandler);
+addBtn.addEventListener('click', addBtnClickHandler);
+
+const nextButtonClickHandler = (event) => {};
+
+const addBtnClickHandler = (event) => {};
